63855b4538cbec188d0007802fbb40d7313f78a4
Zane Wilson <wilsonzanea@gmail.com>
2021-12-10 10:12:34 -0500

    add function timer and Node counter

--- solution.py
+++ solution.py
@@ -3,32 +3,42 @@
 ##############################################################################
 # Implementation of Breadth First Search algorithm
 from collections import deque
+from timeit import default_timer as timer
 
-# Increase the size of the board here
-board = 8
 
-# Don't change below this line!!!!
-board_max = board * board - 1
 
+
+'''The 2 lists below, when iterated through at the same time, mimic the
+    potential movement of a Knight from any Node it's currently at.'''
 row = [2, 2, -2, -2, 1, 1, -1, -1]
 col = [-1, 1, 1, -1, 2, -2, 2, -2]
 
 
-# Initiate the Node object to keep track of where the Knight has moved.
-# This GREATLY cuts down on runtime. The reason is because it stores the moves that have already been made. This way, we can't retrace our steps and every move that is made is a new move, progress towards an answer will be made with every step.
+'''Initiate the Node object to keep track of where the Knight has moved. This
+GREATLY cuts down on runtime. The reason is because it stores the moves that
+have already been made. This way, we can't retrace our steps and every move
+that is made is a new move, progress towards an answer will be made with
+every step.'''
 class Node:
+    count = 0
     def __init__(self, x, y, dist=0):
-        self.x = x       # x coordinate
-        self.y = y       # y coordinate
-        self.dist = dist # tracks amount of moves made to get to current point
+        self.x = x  # x coordinate
+        self.y = y  # y coordinate
+        self.dist = dist  # tracks amount of moves made to get to current point
+
 
 # Check if the coordinates the Knight is going to are valid
 def is_valid(x, y, board):
     return not (x < 0 or y < 0 or x >= board or y >= board)
 
 
-def solution(start, end):
-    if start > board_max or end > board_max or start < 0 or end < 0:
+def solution(start, end, board):
+    # Maximum coordinate on the board
+    board_max = board**2 - 1
+    start_time = timer()
+    try:
+        start > board_max or end > board_max or start < 0 or end < 0
+    except ValueError as err:
         return "Start point or destination point unreachable. Please try again"
     # Convert the start and end to (x, y) coordinates
     start_x, end_x = start // board, end // board
@@ -42,17 +52,21 @@ def solution(start, end):
     q.append(start)
 
     while q:
-        node = q.popleft() # Optimization over q.pop(0)
+        node = q.popleft()  # Optimization over q.pop(0)
         x = node.x
         y = node.y
         dist = node.dist
-        # Jackpot!
+        Node.count += 1
+        # Jackpot! We have a match and time to end.
         if x == end.x and y == end.y:
+            end_time = timer()
+            print(f"There are still {len(q)} potential nodes left for me to check, but I can assure you, this is the shortest path.")
+            print(f"I checked {Node.count} coordinates in {end_time - start_time} seconds.")
             return dist
-        # 
-        if node not in visited:
-            visited.add(node) # More optimization
-            for i in range(8): # iterate over row and col
+
+        if node not in visited: # Check if node has been added already
+            visited.add(node)
+            for i in range(8):  # iterate over row and col
                 x1 = x + row[i]
                 y1 = y + col[i]
                 # call is_valid function to ensure coords are on board
@@ -60,9 +74,11 @@ def solution(start, end):
                     q.append(Node(x1, y1, dist + 1))
 
 
-
-if __name__ == '__main__':
-    print(solution(0, 62))
+if __name__ == "__main__":
+    board = 8
+    start = 0
+    dest = 62
+    print(solution(start, dest, board))
     '''
     Call the function with 2 arguments.
     The arguments you give are coordinates to Point A and Point B.
